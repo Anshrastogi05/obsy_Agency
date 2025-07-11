fc40f0c4981e8417cfa81d8ee4cc2cba77177eb6
Anshrastogi05 <rastogiansh13@gmail.com>
2025-07-12 00:30:52 +0530

    page1 and page2

--- final.js
+++ final.js
@@ -7,9 +7,9 @@ tl.from("#line h1",{
     delay: 0.5
     
 })
-/ tl.from("#line1-part1",{
+ tl.from("#line1-part1",{
     opacity: 0,
-    onstart: function () {
+    onStart: function () {
  var h5timer = document.querySelector("#line1-part1 h5")
    let grow = 0;
 
@@ -57,4 +57,28 @@ ease:Power4
 tl.to("#loader",{
     display:"none"
 })
+tl.from("#nav",{
+   opacity: 0
+})
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2
+})
 }
+
+
+
+function cursorAnimation() {
+document.addEventListener("mousemove",function(dets){
+  gsap.to("#crsr",{
+    left:dets.x,
+    top:dets.y
+  })
+})
+
+Shery.makeMagnet("#nav-part2 h4",{});
+}
+
+loadingAnimation();
+cursorAnimation();
+
